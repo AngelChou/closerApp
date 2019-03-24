f5eccde45f257068fcf01f059eebb67e858c61fc
Angel Chou <cat77120377@hotmail.com>
2019-03-24 15:32:02 +0800

    use segmentedControl to change moving mode

--- closerApp/ViewController.swift
+++ closerApp/ViewController.swift
@@ -10,17 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderModeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var leftImageView: UIImageView!
     @IBOutlet weak var rightImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sliderModeSegmentedControl.selectedSegmentIndex = 0
     }
 
     @IBAction func sliderChanged(_ sender: UISlider) {
-        leftImageView.frame.origin.x = CGFloat(65+sender.value*20)
-        rightImageView.frame.origin.x = CGFloat(605-sender.value*20)
+        
+        let ratio = CGFloat(sender.value/12)
+        // 距離越遠越透明
+        leftImageView.alpha = ratio
+        rightImageView.alpha = ratio
+        
+        let speed = sender.value * 20.0
+        let index = sliderModeSegmentedControl.selectedSegmentIndex
+        switch index {
+        case 0: // 等速水平移動
+            leftImageView.frame.origin.x = CGFloat(65 + speed)
+            rightImageView.frame.origin.x = CGFloat(605 - speed)
+        case 1:// 變速水平移動
+            leftImageView.frame.origin.x = CGFloat(65 + speed * 0.5)
+            rightImageView.frame.origin.x = CGFloat(605 - speed * 1.5)
+            
+        default:
+            return
+        }
     }
-    
 }
 
